6b743b23784201e9e14d7452498d0c59ab9a3b2d
Merey <merey.90@gmail.com>
2017-03-02 17:13:35 +0600

    some minor fixes related to angular-cli upgrade

--- karma.conf.js
+++ karma.conf.js
@@ -6,10 +6,10 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
