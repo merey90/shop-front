7eb51f83143e490495331950aeacf3f4a306df05
Merey <merey.90@gmail.com>
2017-03-29 10:33:17 +0600

    upgrade to ng4

--- src/app/user/user-confirmation/user-confirmation.component.ts
+++ src/app/user/user-confirmation/user-confirmation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SessionService } from '../session/session.service';
+import { SessionService } from './../../session/session.service';
 
 @Component({
   selector: 'app-user-confirmation',
@@ -8,12 +8,12 @@ import { SessionService } from '../session/session.service';
 })
 export class UserConfirmationComponent implements OnInit, OnDestroy {
   private sub: any;
-  account_confirmation_success :boolean = false;
-  private client_id :string = null;
-  private config :string = null;
-  private expiry :number = null;
-  private token :string = null;
-  private uid :string = null;
+  account_confirmation_success: boolean = false;
+  private client_id: string = null;
+  private config: string = null;
+  private expiry: number = null;
+  private token: string = null;
+  private uid: string = null;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -29,7 +29,7 @@ export class UserConfirmationComponent implements OnInit, OnDestroy {
       this.expiry = params['expiry'] || null;
       this.token = params['token'] || null;
       this.uid = params['uid'] || null;
-      if(this.account_confirmation_success && this.token != null && this.token.length > 6){
+      if (this.account_confirmation_success && this.token != null && this.token.length > 6) {
         this.sessionService.validateToken().subscribe(
           res => {
             this.sessionService.email.next(res.json().data.email);
